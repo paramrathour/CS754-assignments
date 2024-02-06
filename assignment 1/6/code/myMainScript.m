26c67d8041fdd096bb18ff6b96d3ac8329beb32b
paramrathour <rathourparam3435@gmail.com>
2024-02-06 18:26:23 +0530

    added directory existance check

--- assignment 1/6/code/myMainScript.m
+++ assignment 1/6/code/myMainScript.m
@@ -7,21 +7,25 @@ m_special = [500 700];
 % k = [5];
 k = [5 10 20 30 50 100 150 200];
 k_special = [5 50 200];
+create_directory("../images")
 process(m, m_special, k, k_special, dimensions, "omp", threshold);
 % process(m, m_special, k, k_special, dimensions, "iht", threshold);
+
 
 function [] = process(m, m_special, k, k_special, dimensions, algorithm, threshold)
     DCT_matrix = dctmtx(dimensions(1)*dimensions(2));
     column_indices = randperm(dimensions(1)*dimensions(2), k(end));
     measurement_matrix = sign(randn(m(end), dimensions(1)*dimensions(2)));
     row_indices = randperm(m(end), m(end));
-    
     RMSE = zeros(length(k), length(m));
+    create_directory("../images/" + algorithm)
+
     for i = 1:length(k)
         % image_vector = sum(DCT_matrix(:, column_indices(1:k(i))), 2);
         image_vector = DCT_matrix(:, column_indices(1:k(i))) * rand(k(i), 1);
         filename = "../images/" + "k =" + string(k(i)) + ".png";
         save_image(image_vector, dimensions, filename);
+        
         for j = 1:length(m)
             phi = measurement_matrix(row_indices(1:m(j)), :);
             image_reconstructed = feval(algorithm, dimensions, phi*image_vector, k(i), phi*DCT_matrix, threshold);
@@ -53,4 +57,10 @@ function [] = save_image(image, dimensions, filename)
     image = image*255/max(image, [], "all");
     image = image_double_to_integer(image);
     imwrite(image, filename);
+end
+
+function [] = create_directory(path)
+    if ~exist(path)
+        mkdir(path)
+    end
 end
