ed82decbc98ff269a0e51d29734dbce7fe7f51e1
paramrathour <rathourparam3435@gmail.com>
2024-03-14 01:04:24 +0530

    added RMSE calculation code

---
+++ assignment 3/6/code/JointHistogram.m
@@ -0,0 +1,15 @@
+% Note: This function is taken from my CS-663 assignments
+
+function array = JointHistogram(I1, I2)
+    array = zeros(26, 26);
+    
+    for i = 1:numel(I1)
+        intensity_x = I1(i);
+        intensity_y = I2(i);
+
+        bin_x = uint8(floor((intensity_x / 10) + 1));
+        bin_y = uint8(floor((intensity_y / 10) + 1));
+
+        array(bin_x, bin_y) = array(bin_x, bin_y) + 1;
+    end
+end
--- assignment 3/6/code/myMainScript.m
+++ assignment 3/6/code/myMainScript.m
@@ -1,6 +1,9 @@
 seed = 0;
 N = [50, 100, 500, 1000, 2000, 5000, 10000];
+N = [50]; % work with this for testing
+RMSE_info = "";
 image_name = ["cryoem"];
+angles_rotation = 0:359;
 
 rng(seed);
 angles = 360*rand(max(N), 1);
@@ -10,18 +13,47 @@ for image = image_name
         image_input = imread("../results/"+ image +".png");
         image_input = double(image_input);
         projections = generateProjections(image_input, angles(1:n));
-        image_output = reconstructImage(projections, angles(1:n));
+        image_output = reconstructImage(projections, angles(1:n), image_input);
+        [RMSE, image_reconstructed, angle_rotated] = calculate_RMSE(image_input, image_output, angles_rotation);
+        if angle_rotated <= length(angles_rotation);
+            reflected = "";
+        else
+            reflected = ", reflected";
+        end
+        angle = mod(angle_rotated, length(angles_rotation));
+        if angle ~= 0
+            angle = angle - 1;
+            angle = 360 - angle;
+        end
         save_image(image_output, "../results/"+image+", N = "+string(n)+".png");
+        save_image(image_reconstructed, "../results/"+image+", N = "+string(n)+reflected+" and rotated by angle = "+string(angle)+".png");
+        RMSE_info = RMSE_info + "RMSE = " + string(RMSE) + " for " + image_name + " and, N = "+string(n) + newline;
     end
 end
+file = fopen('../results/RMSE.txt', 'wt');
+fprintf(file, RMSE_info);
+fclose(file);
 
 function projections = generateProjections(image_input, angles)
     % returns a matrix where each column is a radon projections for corresponding angle
      projections = radon(image_input, angles);
 end
 
-function image_output = reconstructImage(projections, angles)
+function image_output = reconstructImage(projections, angles, image_input)
     % nearest neighbour algorithm
+end
+
+function [RMSE, image_reconstructed, argmaximum] = calculate_RMSE(image_original, image_output, angles_rotation)
+    % Note: This function is inspired from my CS-663 assignment 1
+    image_output_flipped = fliplr(image_output);
+    QMI_values = [arrayfun(@(angle) QMI(image_original, imrotate(image_output, angle, "bilinear", "crop")), angles_rotation) arrayfun(@(angle) QMI(image_original, imrotate(image_output_flipped, angle, "bilinear", "crop")), angles_rotation)];
+    [maximum, argmaximum] = max(QMI_values);
+    if argmaximum <= length(angles_rotation)
+        image_reconstructed = imrotate(image_output, angles_rotation(argmaximum), "bilinear", "crop");
+    else
+        image_reconstructed = imrotate(image_output_flipped, angles_rotation(argmaximum-length(angles_rotation)), "bilinear", "crop");
+    end
+    RMSE = norm(image_reconstructed(:) - image_original(:)) / norm(image_original(:));
 end
 
 function image_input = image_bound(image_input)
